5b0b03458703a68b571783c36dfae307e1ff5557
Vitaly <ksainer@mail.ru>
2019-09-13 12:55:33 +0300

    add form sign-in/sign-up, add user-profile

--- yourblog/js/main.js
+++ yourblog/js/main.js
@@ -1,165 +1,30 @@
-// top-stories slider
-
-const slider = document.body.querySelector('.slider');
-let sliderWidth = slider.clientWidth;
-const sliderList = slider.querySelector('.slider__list');
-const btnPrev = slider.querySelector('.slider__btn-prev');
-const btnNext = slider.querySelector('.slider__btn-next');
-// options for slider:
-let position = 0;
-const widthItem = sliderList.firstElementChild.clientWidth;
-let count = Math.floor(sliderWidth / widthItem);
-count = Math.max(1, Math.min(2, count))
-
-btnPrev.hidden = true;
-
-btnPrev.onclick = function() {
-	position += widthItem * count;
-	if (position > -5) {
-		position = 0;
-		btnPrev.hidden = true;
-	}
-	sliderList.style.transform = `translateX(${position}px)`;
-	btnNext.hidden = false;
-}
-
-btnNext.onclick = function() {
-	position -= widthItem * count;
-	if (position < -(sliderList.scrollWidth - sliderWidth - 5)){
-		position = - (sliderList.scrollWidth - sliderWidth + 30);
-		btnNext.hidden = true;
-	}
-	sliderList.style.transform = `translateX(${position}px)`;
-	btnPrev.hidden = false;
-}
-
-function calculateSlider() {
-	sliderWidth = slider.clientWidth;
-	count =  Math.floor(sliderWidth / widthItem);
-	count = Math.max(1, Math.min(2, count));
-	if (position < -(sliderList.scrollWidth - sliderWidth - 5)){
-		position = - (sliderList.scrollWidth - sliderWidth + 30);
-		btnNext.hidden = true;
-	} else btnNext.hidden = false;
-	sliderList.style.transform = `translateX(${position}px)`;
-}
-
-calculateSlider = throttle(calculateSlider, 200);
-
-window.addEventListener("resize", () => {
-	calculateSlider();
-})
-
-// set color for slider topic
-const sliderTopics = sliderList.querySelectorAll('.slider__topic');
-const colors = {
-	Cinema: 		'#5261ac', // purple
-	Travel: 		'#8cc63f', // green
-	Television: '#ec098d', //pink
-	Music: 		'#f78f1d', // orange
-	"TV Show": 	'#00aedb', // blue
-}
-for (let topic of sliderTopics) {
-	if (colors[topic.innerHTML.trim()])
-		topic.style.backgroundColor = colors[topic.innerHTML.trim()];
-}
-
-// disable tab for slider items
-for (let item of sliderList.children) { 
-	item.firstElementChild.setAttribute('tabindex', '-1');
-}
-
-// =================================================
-// =================================================
-
-document.addEventListener("DOMContentLoaded", function() {
-	// gallery for posts
-	const galleryList = document.body.querySelectorAll('.gallery__list');
-
-	function calculateGeometryImgGallery(listImg) {
-		for (let j = 0; j < listImg.length; j++) {
-			const items = listImg[j].children;
-			const countImg = items.length < 5 ? 4 : 5;
-			const width = listImg[j].clientWidth / countImg  + 'px';
-		
-			for (let i = 0; i < items.length; i++) {
-				items[i].style.height = width;
-				items[i].style.width = width;
-		
-				if (i && !((i + 1) % countImg)) {
-					items[i].style.marginRight = 0;
-				}	
-			}
-		}
-	}
-
-	calculateGeometryImgGallery(galleryList);
-	
-	calculateGeometryImgGallery = throttle(calculateGeometryImgGallery, 1000);
-	
-	for (let i = 0; i < galleryList.length; i++) {
-		let currentImg = galleryList[i].parentElement.querySelector('.gallery__current-img');
-		
-		galleryList[i].onclick = function() {
-			let smallImg = event.target;
-			if (smallImg.tagName == 'IMG') {
-				let newSrc = smallImg.dataset.bigsize;
-				currentImg.setAttribute('src', newSrc);
-			}
-		}
-	}
-	
-	window.addEventListener('resize', () => {
-		calculateGeometryImgGallery(galleryList);
-	})
-	//===============================================
-
+document.addEventListener("DOMContentLoaded", () => {
 	// background lines resizing
 	const bgLines = document.body.querySelector('.lines');
 
 	function transformLines() {
-		bgLines.style.transform = `rotate(30deg) scaleX(${
-			1 + (document.documentElement.scrollTop 
-			/ document.documentElement.clientHeight) * 2
-		})`;
+		let scrollTop = document.documentElement.scrollTop;
+		let clientHeight = document.documentElement.clientHeight;
+		let clientWidth = document.documentElement.clientWidth;
+
+		if (scrollTop + 200 < clientWidth)
+			bgLines.style.transform = `rotate(30deg) scaleX(${	1 + (scrollTop / clientHeight) * 2 })`;
 	}
 
 	transformLines = throttle(transformLines, 100);
 
-	window.addEventListener('scroll', () => {
-		transformLines();
-	})
+	window.addEventListener('scroll', () => transformLines());
 	// ==============================================
 
-	// create tag cloud 
-	function randomProperty(tag) {
-		// min + Math.random() * (max + 1 - min) => [13...28]
-		tag.style.fontSize = Math.floor(13 + Math.random() * 17) + 'px';
-		tag.style.fontWeight = Math.random() < 0.3 ? 'bold' : '';
-		tag.style.color = Math.random() < 0.15 ? '#f78f1d' : '';
-	}
-	const listTag = document.body.querySelectorAll('.tag-cloud__item');
-	listTag.forEach(item => randomProperty(item));
-	// ==============================================
+	// scrim config
+	const scrim = document.body.querySelector('.scrim');
+	let scrimKey;
 
-	// last-stories change category
-	const lastStories = document.body.querySelector('.last-stories');
-
-	lastStories.onclick = function(e) {
-		const target = e.target;
-
-		const topic = target.dataset.topic;
-		
-		if (!topic || target.classList.contains('active-category')) return;
-		
-		const activeCategory = lastStories.querySelector('.active-category');
-		const activeTopic = activeCategory.dataset.topic;
-
-		activeCategory.classList.remove('active-category');
-		lastStories.querySelector('.topic__' + activeTopic).style.display = 'none';
-		
-		target.classList.add('active-category');
-		lastStories.querySelector('.topic__' + topic).style.display = 'block';
+	scrim.onclick = function() {
+		switch (scrimKey) {
+			case "sidebar": hideSidebar(); break;
+			case "signIn": hideSignIn(); break;
+		}
 	}
 	// ================================================
 
@@ -183,47 +48,261 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	showbtnToUp = throttle(showbtnToUp, 500);
 
-	window.addEventListener('scroll', () => {
-		showbtnToUp();
-	})
+	window.addEventListener('scroll', () => showbtnToUp());
 	// ================================================
-	
-	// button show and hide sidebar
-	const btnSidebarShow = document.body.querySelector('.button-sidebar');
-	const sidebar = document.body.querySelector('.sidebar');
-	const sidebarWrap = sidebar.querySelector('.sidebar__wrap');
-	const scrim = document.body.querySelector('.scrim');
-	scrim.hidden = true;
 
-	function sidebarShow() {
-		btnSidebarShow.classList.toggle('back-arrow');
-		sidebar.classList.toggle('sidebar-show');
-		scrim.hidden = scrim.hidden ? false : true;
-		if (sidebarWrap.style.display == 'block') 
-			sidebarWrap.style.display = 'none'; 
-		else sidebarWrap.style.display = 'block';
+	setColors(document.body.querySelector('.main-menu').children);
+
+	// sign-in__button
+	const sectionLogin = document.body.querySelector('.auth');
+	if (sectionLogin) {
+		const signIn = document.body.querySelector('.sign-in__button');
+		const sectionLoginCloseButton = sectionLogin.querySelector('.auth__close-button');
+		sectionLoginCloseButton.onclick = hideSignIn;
+		signIn.onclick = showSignIn;
+
+		function showSignIn() {
+			scrim.style.zIndex = '6';
+			scrim.hidden = false;
+			sectionLogin.hidden = false;
+			scrimKey = 'signIn';
+			sectionLogin.querySelector('input').focus();
+		}
+
+		function hideSignIn() {
+			scrim.style.zIndex = '';
+			scrim.hidden = true;
+			sectionLogin.hidden = true;
+			scrimKey = '';
+		}
+		
+		const sectionLoginSignIn = sectionLogin.querySelector('.auth__sign-in');
+		const sectionLoginSignUp = sectionLogin.querySelector('.auth__sign-up');
+		const formSignIn = sectionLogin.querySelector('.form-sign-in');
+		const formSignUp = sectionLogin.querySelector('.form-sign-up');
+
+		function changeLogin() {
+			if (!this.classList.contains('auth__active')) {
+				sectionLoginSignIn.classList.toggle('auth__active');
+				sectionLoginSignUp.classList.toggle('auth__active');
+				// this.classList.add('auth__active');
+
+				formSignIn.classList.toggle('visible');
+				formSignUp.classList.toggle('visible');
+
+				// formSignIn.style.display = getComputedStyle(formSignIn).display == 'none' ? 'block' : 'none';
+				// formSignUp.style.display = getComputedStyle(formSignUp).display == 'none' ? 'block' : 'none';
+			}
+		}
+
+		sectionLoginSignIn.onclick = sectionLoginSignUp.onclick = changeLogin;
 	}
-	btnSidebarShow.onclick = sidebarShow;
+	// ================================================
 
-	scrim.onclick = sidebarShow;
+	// account
+	const account = document.body.querySelector('.account');
+	if (account) {
+		const accountButton = account.querySelector('.account__button');
+		const accountMenu = account.querySelector('.account__menu');
+
+		setColors(accountMenu.children);
+
+		accountButton.onclick = function() {
+			accountMenu.classList.toggle('visible');
+		}
+
+		document.addEventListener('click', function(e) {
+			if (!account.contains(e.target) && accountMenu.classList.contains('visible'))
+				accountMenu.classList.remove('visible');
+		})
+		document.addEventListener('keydown', () => {
+			if (event.key == 'Escape' && accountMenu.classList.contains('visible'))
+				accountMenu.classList.remove('visible');
+		});
+	}
+	// ================================================
+
+	// top-stories slider
+	const slider = document.body.querySelector('.slider');
+	if (slider) {
+		let sliderWidth = slider.clientWidth;
+		const sliderList = slider.querySelector('.slider__list');
+		const btnPrev = slider.querySelector('.slider__btn-prev');
+		const btnNext = slider.querySelector('.slider__btn-next');
+		// options for slider:
+		let position = 0;
+		const widthItem = sliderList.firstElementChild.clientWidth;
+		let count = Math.floor(sliderWidth / widthItem);
+		count = Math.max(1, Math.min(2, count))
+
+		btnPrev.hidden = true;
+
+		btnPrev.onclick = function() {
+			position += widthItem * count;
+			if (position > -5) {
+				position = 0;
+				btnPrev.hidden = true;
+			}
+			sliderList.style.transform = `translateX(${position}px)`;
+			btnNext.hidden = false;
+		}
+
+		btnNext.onclick = function() {
+			position -= widthItem * count;
+			if (position < -(sliderList.scrollWidth - sliderWidth - 5)){
+				position = - (sliderList.scrollWidth - sliderWidth + 30);
+				btnNext.hidden = true;
+			}
+			sliderList.style.transform = `translateX(${position}px)`;
+			btnPrev.hidden = false;
+		}
+
+		function calculateSlider() {
+			sliderWidth = slider.clientWidth;
+			count =  Math.floor(sliderWidth / widthItem);
+			count = Math.max(1, Math.min(2, count));
+			if (position < -(sliderList.scrollWidth - sliderWidth - 5)){
+				position = - (sliderList.scrollWidth - sliderWidth + 30);
+				btnNext.hidden = true;
+			} else btnNext.hidden = false;
+			sliderList.style.transform = `translateX(${position}px)`;
+		}
+
+		calculateSlider = throttle(calculateSlider, 200);
+
+		window.addEventListener("resize", () => calculateSlider());
+
+		// set color for slider topic
+		const sliderTopics = sliderList.querySelectorAll('.slider__topic');
+		setColors(sliderTopics, true);
+
+		// disable tab for slider items
+		for (let item of sliderList.children) { 
+			item.firstElementChild.setAttribute('tabindex', '-1');
+		}
+	}
+	// =================================================
+
+	// gallery for posts
+	const galleryList = document.body.querySelectorAll('.gallery__list');
+	if (galleryList) {
+		function calculateGeometryImgGallery(listImg) {
+			for (let j = 0; j < listImg.length; j++) {
+				const items = listImg[j].children;
+				const countImg = items.length < 5 ? 4 : 5;
+				const width = listImg[j].clientWidth / countImg  + 'px';
+			
+				for (let i = 0; i < items.length; i++) {
+					items[i].style.height = width;
+					items[i].style.width = width;
+			
+					if (i && !((i + 1) % countImg)) {
+						items[i].style.marginRight = 0;
+					}	
+				}
+			}
+		}
+
+		calculateGeometryImgGallery(galleryList);
+		
+		calculateGeometryImgGallery = throttle(calculateGeometryImgGallery, 1000);
+		
+		for (let i = 0; i < galleryList.length; i++) {
+			let currentImg = galleryList[i].parentElement.querySelector('.gallery__current-img');
+			
+			galleryList[i].onclick = function() {
+				let smallImg = event.target;
+				if (smallImg.tagName == 'IMG') {
+					let newSrc = smallImg.dataset.bigsize;
+					currentImg.setAttribute('src', newSrc);
+				}
+			}
+		}
+		
+		window.addEventListener('resize', () => {
+			if (window.innerWidth < 1100)	calculateGeometryImgGallery(galleryList);
+		})
+	}
+	//===============================================
+
+	// last-stories change category
+	const lastStories = document.body.querySelector('.last-stories');
+
+	lastStories.onclick = function(e) {
+		const target = e.target;
+
+		const topic = target.dataset.topic;
+		
+		if (!topic || target.classList.contains('active-category')) return;
+		
+		const activeCategory = lastStories.querySelector('.active-category');
+		const activeTopic = activeCategory.dataset.topic;
+
+		activeCategory.classList.remove('active-category');
+		lastStories.querySelector('.topic__' + activeTopic).style.display = 'none';
+		
+		target.classList.add('active-category');
+		lastStories.querySelector('.topic__' + topic).style.display = 'block';
+	}
+	// ================================================
+
+	// button show and hide sidebar
+	const sidebar = document.body.querySelector('.sidebar');
+	const toggleButtonSidebar = sidebar.querySelector('.button-sidebar');
+	const sidebarWrap = sidebar.querySelector('.sidebar__wrap');
+
+	setColors(sidebar.querySelector('.last-stories__menu').children, true);
+	
+	function showSidebar() {
+		toggleButtonSidebar.classList.add('back-arrow');
+		sidebar.classList.add('sidebar-show');
+		sidebarWrap.style.display = 'block';
+		scrim.hidden = false;
+		scrimKey = 'sidebar';
+	}
+
+	function hideSidebar() {
+		toggleButtonSidebar.classList.remove('back-arrow');
+		sidebar.classList.remove('sidebar-show');
+		sidebarWrap.style.display = '';
+		scrim.hidden = true;
+		scrimKey = '';
+	}	
+
+	toggleButtonSidebar.onclick = function sidebarToggle() {
+		scrimKey == 'sidebar' ? hideSidebar() : showSidebar();
+	};
 
 	function resetSidebar()  {
-		if (window.innerWidth > 1000) {
-			sidebarWrap.style = 'block';
-			sidebar.classList.remove('sidebar-show');
-			btnSidebarShow.classList.remove('back-arrow');
-			scrim.hidden = true;
-			setTimeout(() => calculateGeometryImgGallery(galleryList), 400); 
-		} 
+		if (scrimKey == 'sidebar') hideSidebar();
+		setTimeout(() => calculateGeometryImgGallery(galleryList), 400);
 	}
 	
 	resetSidebar = throttle(resetSidebar, 200);
 
-	window.addEventListener("resize", () => resetSidebar());
+	window.addEventListener("resize", () =>{
+		if (window.innerWidth > 1000) resetSidebar();
+	});
 	// ================================================
+
+	// create tag cloud 
+	function randomProperty(tag) {
+		// min + Math.random() * (max + 1 - min) => [13...28]
+		tag.style.fontSize = Math.floor(13 + Math.random() * 17) + 'px';
+		tag.style.fontWeight = Math.random() < 0.3 ? 'bold' : '';
+		tag.style.color = Math.random() < 0.15 ? '#f78f1d' : '';
+	}
+	const listTag = document.body.querySelectorAll('.tag-cloud__item');
+	listTag.forEach(item => randomProperty(item));
+	// ==============================================
+
+	// hot keys
+	document.addEventListener('keydown', (event) => {
+		if (event.key == 'Escape' && !scrim.hidden) scrim.click();
+	});
+	// ==============================================
 });
 
-// throttle function
 function throttle(f, ms) {
 	let delay = true;
 	let saveArgs = null;
@@ -242,6 +321,44 @@ function throttle(f, ms) {
 		} else {
 			saveArgs = arguments;
 		}
+	}
+}
+
+function setColors(elemList, byName) {
+	const colors = [
+		'#00aedb', // blue
+		'#8cc63f', // green
+		'#ec098d', // pink
+		'#f78f1d', // orange
+		'#FF0000', // red
+		// '#FF0000', // red
+	];
+	const colorTopics = {
+		"tv show": 	0, // blue
+		cinema: 		0, // blue
+		travel: 		1, // green
+		fashion: 	2, // pink
+		television: 2, // pink
+		sport:	 	3, // orange
+		music: 		4, // red
+	};
+	
+	if (byName) {
+
+		for (let i = 0; i < elemList.length; ++i) {
+			let text = elemList[i].innerHTML.trim().toLowerCase();
+			let color = colors[colorTopics[text]]
+			if (color) 
+				elemList[i].style.backgroundColor = color;
+		}
+
+	} else {
+
+		for (let i = 0; i < elemList.length; ++i) {
+			let color = colors[i - colors.length * Math.floor( i / colors.length)];
+			elemList[i].style.backgroundColor = color;
+		}	
+
 	}
 }
 
